a10e8a1e34feb2f27b46492f071621249711d6d0
Naoya Yamashita <conao3@gmail.com>
2022-11-02 23:59:22 +0900

    clippy

--- src/compiler.rs
+++ src/compiler.rs
@@ -13,35 +13,36 @@ pub fn compile(
 fn comp(
     exp: &types::RuccoExpRef,
     arena: &mut types::RuccoArena,
-    env: &mut core::RuccoEnv,
+    _env: &mut core::RuccoEnv,
     code: &types::RuccoExpRef,
 ) -> anyhow::Result<types::RuccoExpRef> {
-    let exp_ptr = exp.upgrade().ok_or(types::RuccoRuntimeErr::InvalidReference)?;
+    let exp_ptr = exp
+        .upgrade()
+        .ok_or(types::RuccoRuntimeErr::InvalidReference)?;
     let ldc = arena.alloc(types::RuccoExp::new_symbol("ldc"));
     let x = match &*exp_ptr.borrow() {
         types::RuccoExp::Atom(ref atom) => match atom {
-            types::RuccoAtom::Symbol(ref symbol) => {
+            types::RuccoAtom::Symbol(ref _sym) => {
                 unimplemented!()
             }
-            ref _atom => {
-                Ok(arena.alloc_dotlist(vec![&ldc, &exp, &code]))
-            }
+            _atom => Ok(arena.alloc_dotlist(vec![&ldc, exp, code])),
         },
-        types::RuccoExp::Cons {
-            car: ref car,
-            cdr: ref cdr,
-        } => {
-            let car_ptr = car.upgrade().ok_or(types::RuccoRuntimeErr::InvalidReference)?;
-            let cdr_ptr = cdr.upgrade().ok_or(types::RuccoRuntimeErr::InvalidReference)?;
+        types::RuccoExp::Cons { ref car, ref cdr } => {
+            let car_ptr = car
+                .upgrade()
+                .ok_or(types::RuccoRuntimeErr::InvalidReference)?;
+            let cdr_ptr = cdr
+                .upgrade()
+                .ok_or(types::RuccoRuntimeErr::InvalidReference)?;
             let x = match &*car_ptr.borrow() {
                 types::RuccoExp::Atom(ref atom) => match atom {
                     types::RuccoAtom::Symbol(ref sym) if sym == "quote" => {
-                        Ok(arena.alloc_dotlist(vec![&ldc, cdr_ptr.borrow().car_weak_ref()?, &code]))
+                        Ok(arena.alloc_dotlist(vec![&ldc, cdr_ptr.borrow().car_weak_ref()?, code]))
                     }
-                    types::RuccoAtom::Symbol(ref sym) => unimplemented!(),
+                    types::RuccoAtom::Symbol(ref _sym) => unimplemented!(),
                     _ => unimplemented!(),
-                }
-                types::RuccoExp::Cons {..} => unimplemented!()
+                },
+                types::RuccoExp::Cons { .. } => unimplemented!(),
             };
             x
         }
